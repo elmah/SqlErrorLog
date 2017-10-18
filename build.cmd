cb66f77d17cf34caadc79e897a99a7ff6467635d
Atif Aziz <code@raboof.com>
2017-10-18 14:08:42 +0200

    Upgrade to VS 2017 & MSBuild 15

--- build.cmd
+++ build.cmd
@@ -7,5 +7,5 @@ goto :EOF
 :main
 setlocal
 nuget restore ^
- && (for %%v in (3.5 4.0 4.5) do for %%c in (Debug Release) do MSBuild "/p:Configuration=NETFX %%v %%c" /v:m %* || exit /b 1)
+ && (for %%v in (3.5 4.0 4.5) do for %%c in (Debug Release) do call msbuild "/p:Configuration=NETFX %%v %%c" /v:m %* || exit /b 1)
 goto :EOF
